c4a6f152d36791f612532cde6967c1e8c3baea6e
Malcolm Nunley <Mxnunley1@gmail.com>
2020-04-04 11:40:58 -0400

    first commit

---
+++ src/components/TaskForm.js
@@ -0,0 +1,55 @@
+import React, { useContext, useState, useEffect } from "react";
+import { TaskListContext } from "../context/TaskListContext";
+
+const TaskForm = () => {
+  const { addTask, clearList, editItem, editTask } = useContext(
+    TaskListContext
+  );
+
+  const [title, setTitle] = useState("");
+
+  const handelChange = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!editItem) {
+      addTask(title);
+      setTitle("");
+    } else {
+      editTask(title, editItem.id);
+    }
+  };
+
+  useEffect(() => {
+    if (editItem) {
+      setTitle(editItem.title);
+    } else {
+      setTitle("");
+    }
+  }, [editItem]);
+
+  return (
+    <form onSubmit={handleSubmit} className="form">
+      <input
+        onChange={handelChange}
+        value={title}
+        type="text"
+        className="task-input"
+        placeholder="Add Task..."
+        required
+      />
+      <div className="buttons">
+        <button type="submit" className="btn add-task-btn">
+          {editItem ? "Edit Task" : "Add Task"}
+        </button>
+        <button onClick={clearList} type="submit" className="btn clear-btn">
+          Clear
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default TaskForm;
